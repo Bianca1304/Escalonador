eb68b09deb086966d8cf198785eba28262b097ff
Bianca Costa <bibixcosta1@gmail.com>
2019-09-25 09:55:17 -0300

    Problemas concertados depois do merge

--- src/main/java/br/ufpb/dcx/aps/escalonador/FachadaEscalonador.java
+++ src/main/java/br/ufpb/dcx/aps/escalonador/FachadaEscalonador.java
@@ -1,8 +1,16 @@
 package br.ufpb.dcx.aps.escalonador;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FachadaEscalonador {
-	
+
 	private int tick;
+	private List<String> fila = new ArrayList<String>();
+	private String rodando;
+	private List<Integer> filaDuracao = new ArrayList<Integer>();
+	private int duracaoRodando;
+	private int duracaoFixa;
 
 	public FachadaEscalonador(TipoEscalonador tipoEscalonador) {
 	}
@@ -11,14 +19,45 @@ public class FachadaEscalonador {
 	}
 
 	public String getStatus() {
-		return "Escalonador RoundRobin;Processos: {};Quantum: 3;Tick: " + tick;
+		// MaisCurtoPrimeiro;Processos: {Fila: [P1]};Quantum: 0;Tick: 0
+		if (fila.size() == 0) {
+			if (rodando != null) {
+				return "Escalonador MaisCurtoPrimeiro;Processos: {Rodando: " + rodando + "};Quantum: 0;Tick: " + tick;
+			}
+			return "Escalonador MaisCurtoPrimeiro;Processos: {};Quantum: 0;Tick: " + tick;
+		} else {
+			return "Escalonador MaisCurtoPrimeiro;Processos: {Fila: " + fila + "};Quantum: 0;Tick: " + tick;
+		}
 	}
 
 	public void tick() {
 		tick++;
+		if (fila.size() > 0) {
+			if (rodando == null) {
+				rodando = fila.remove(0);
+				duracaoRodando = filaDuracao.remove(0);
+				duracaoFixa = this.tick + duracaoRodando;
+			}
+
+		}
+		if (duracaoFixa == this.tick && rodando != null) {
+			if (fila.size() > 0) {
+				rodando = fila.remove(0);
+				duracaoRodando = filaDuracao.remove(0);
+			}
+			else {
+				rodando = null;
+				duracaoRodando = 0;
+			}
+			if(duracaoRodando > 0) {
+				duracaoFixa = tick + duracaoRodando;
+				
+			}
+		}
 	}
 
 	public void adicionarProcesso(String nomeProcesso) {
+
 	}
 
 	public void adicionarProcesso(String nomeProcesso, int prioridade) {
@@ -31,10 +70,17 @@ public class FachadaEscalonador {
 	}
 
 	public void retomarProcesso(String nomeProcesso) {
-		
+
 	}
 
-	public void adicionarProcessoTempoFixo(String string, int duracao) {
+	public void adicionarProcessoTempoFixo(String nomeProcesso, int duracao) {
 		
+		if(fila.size()==0) {
+			fila.add(nomeProcesso);
+			filaDuracao.add(duracao);
+		}
+		else {
+			
+		}
 	}
 }
